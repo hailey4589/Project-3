2a90c98f0bd63755a92784c7156e59a1bf560e93
hailey3456 <119626099+hailey3456@users.noreply.github.com>
2023-12-08 22:52:56 -0500

    done
    done masking looking great finally!!!!

--- Masking.py
+++ Masking.py
@@ -1,41 +1,65 @@
 
 import cv2 
-import numpy
+import numpy as np
+
 
 
 
 
 image = cv2.imread('motherboard_image.JPEG')
-cv2.imshow("1", image)
-cv2.waitKey(100000)
-cv2.destroyAllWindows()
+image = cv2.resize(image, (800, 600))
 
 # Convert the image to grayscale
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # Apply GaussianBlur to reduce noise and improve edge detection
-blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+blurred = cv2.GaussianBlur(image,(5, 5), 0)
 
-# Apply adaptive thresholding to create a binary image
-thresholded = cv2.threshold(image, 150, 255, cv2.THRESH_BINARY)
+#Apply adaptive thresholding to create a binary image
+#timage = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 8)
 
-# Apply Canny edge detection
-edges = cv2.Canny(image, 30, 150)
+# FIRST ITERATION 
+#  edge detection
+edges = cv2.Canny(gray ,80, 220)
 
-# Find contours in the edged image
-contours, _ = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+lines = cv2.HoughLinesP(edges, 1, np.pi / 180, threshold=50, minLineLength=50, maxLineGap=12)
 
-# Filter contours based on area (adjust the area threshold accordingly)
-min_contour_area = 1000
-filtered_contours = [contour for contour in contours if cv2.contourArea(contour) > min_contour_area]
+if lines is not None:
+        # Create a mask for the detected lines
+        line_mask = np.zeros_like(edges)
+        for line in lines:
+            x1, y1, x2, y2 = line[0]
+            cv2.line(line_mask, (x1, y1), (x2, y2), 255, 2)
+kernel = np.ones((38, 38), np.uint8)
+line_mask = cv2.morphologyEx(line_mask, cv2.MORPH_CLOSE, kernel)
+kernel = np.ones((25, 25), np.uint8)
+line_mask = cv2.morphologyEx(line_mask, cv2.MORPH_OPEN, kernel)
 
-# Draw contours on the original image
-cv2.drawContours(image, filtered_contours, -1, (0, 255, 0), 2)
+result = cv2.bitwise_and(image, image, mask=line_mask)
+
+# SECOND TIME 
+edges = cv2.Canny(result ,150, 255)
+lines = cv2.HoughLinesP(edges, 1, np.pi / 180, threshold=50, minLineLength=50, maxLineGap=15)
+if lines is not None:
+        # Create a mask for the detected lines
+        line_mask = np.zeros_like(edges)
+        for line in lines:
+            x1, y1, x2, y2 = line[0]
+            cv2.line(line_mask, (x1, y1), (x2, y2), 255, 2)
+result1 = cv2.bitwise_and(result, result, mask=line_mask)
+
+kernel = np.ones((45, 45), np.uint8)
+line_mask = cv2.morphologyEx(line_mask, cv2.MORPH_CLOSE, kernel)
+kernel = np.ones((25, 25), np.uint8)
+line_mask = cv2.morphologyEx(line_mask, cv2.MORPH_OPEN, kernel)
+
+result = cv2.bitwise_and(image, image, mask=line_mask)
 
 # Display the results
-#cv2.imshow("Original Image", image)
-cv2.imshow("Thresholded Image", thresholded)
-#cv2.imshow("Edges", edges)
+cv2.imshow("Original Image",image)
+cv2.imshow(" Image", result)
+cv2.imshow("Edges", edges )
 cv2.waitKey(1000000000)
 cv2.destroyAllWindows()
+cv2.waitKey(1)  # Add a small delay (1 millisecond) to allow windows to close
 
